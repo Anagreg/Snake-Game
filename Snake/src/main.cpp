f8ea5424a5ee5a78207c2473ade670e7bae683a1
Anagreg <60714204+Anagreg@users.noreply.github.com>
2020-07-08 15:38:34 +0300

    Update main.cpp

--- Snake/src/main.cpp
+++ Snake/src/main.cpp
@@ -1,30 +1,3 @@
-//#include "Game.h"
-//
-//int main(int argc, char* argv[]) {
-//    srand(time(NULL));
-//    const int frameRate = 5;
-//    const int frameDelay = 1000 / frameRate;
-//
-//    Uint32 frameTime;
-//    int frameElapsedTime;
-//
-//    Game* game = new Game();
-//    game->init("Snake game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, G_SIZE, G_SIZE, false);
-//    while (game->isRunning()) {
-//        frameTime = SDL_GetTicks();
-//
-//        game->handleEvents();
-//        game->update();
-//        game->render();
-//
-//        frameElapsedTime = SDL_GetTicks() - frameTime;
-//        if (frameDelay > frameElapsedTime)
-//            SDL_Delay(frameDelay - frameElapsedTime);
-//    }
-//    game->clean();
-//    return 0;
-//}
-
 #include <iostream>
 #include "Game.h"
 #include "Renderer_SDL.h"
